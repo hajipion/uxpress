a15de10b355f61b712d8a79ad2b51e2c10e7e685
hajipion <hajiming@gmail.com>
2014-10-06 00:51:41 +0900

    Add tmp nav-prev-post & nav-next-post

--- functions.php
+++ functions.php
@@ -1,4 +1,7 @@
 <?php
+
+	// サムネイル
 	add_theme_support( 'post-thumbnails', array( 'post' ) );
 	remove_action('wp_head','wp_generator');
+
 ?>
---
+++ tmp-to-single.php
@@ -0,0 +1,9 @@
+<hr class="horizon-gray">
+<div class="link-to-single">
+	<div class="next-post-link">
+		<?php next_post_link('&laquo; %link'); ?>
+	</div>
+	<div class="prev-post-link">
+		<?php previous_post_link('%link &raquo;'); ?>
+	</div>
+</div>
