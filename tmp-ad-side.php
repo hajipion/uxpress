7fa3420cefcb99d810b296ef297b5b0ef78dd432
hajipion <hajiming@gmail.com>
2014-12-15 08:37:51 +0900

    New ad style

--- tmp-ad-side.php
+++ tmp-ad-side.php
@@ -1,24 +1,14 @@
 <div class="side-ad-area">
 	<div class="ad-side">
-		<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-		<!-- sidewide -->
-		<ins class="adsbygoogle"
-		     style="display:inline-block;width:160px;height:600px"
-		     data-ad-client="ca-pub-1325377879579480"
-		     data-ad-slot="4670381259"></ins>
-		<script>
-		(adsbygoogle = window.adsbygoogle || []).push({});
-		</script>
+		<iframe src="http://rcm-fe.amazon-adsystem.com/e/cm?lt1=_blank&bc1=C3C3C3&IS2=1&nou=1&bg1=FFFFFF&fc1=000000&lc1=5199EF&t=hajipion-22&o=9&p=8&l=as1&m=amazon&f=ifr&ref=qf_sp_asin_til&asins=4883378152" style="width:237px;height:240px;" scrolling="no" marginwidth="0" marginheight="0" frameborder="0"></iframe>
 	</div>
 	<div class="ad-side">
-		<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-		<!-- sidewide -->
-		<ins class="adsbygoogle"
-		     style="display:inline-block;width:160px;height:600px"
-		     data-ad-client="ca-pub-1325377879579480"
-		     data-ad-slot="4670381259"></ins>
-		<script>
-		(adsbygoogle = window.adsbygoogle || []).push({});
-		</script>
+		<iframe src="http://rcm-fe.amazon-adsystem.com/e/cm?lt1=_blank&bc1=C3C3C3&IS2=1&bg1=FFFFFF&fc1=000000&lc1=5199EF&t=hajipion-22&o=9&p=8&l=as1&m=amazon&f=ifr&ref=tf_til&asins=4478016380" style="width:237px;height:240px;" scrolling="no" marginwidth="0" marginheight="0" frameborder="0"></iframe>
+	</div>
+	<div class="ad-side">
+		<iframe src="http://rcm-fe.amazon-adsystem.com/e/cm?lt1=_blank&bc1=C3C3C3&IS2=1&bg1=FFFFFF&fc1=000000&lc1=5199EF&t=hajipion-22&o=9&p=8&l=as1&m=amazon&f=ifr&ref=tf_til&asins=B00MVLKM2W" style="width:237px;height:240px;" scrolling="no" marginwidth="0" marginheight="0" frameborder="0"></iframe>
+	</div>
+	<div class="ad-side">
+		<iframe src="http://rcm-fe.amazon-adsystem.com/e/cm?t=hajipion-22&o=9&p=11&l=ur1&category=amazonsale&f=ifr" width="237" height="600" scrolling="no" border="0" marginwidth="0" style="border:none;" frameborder="0"></iframe>
 	</div>
 </div>
--- tmp-ad-single.php
+++ tmp-ad-single.php
@@ -1,24 +1,8 @@
-<!--
 <div class="single-ad-area">
 	<div class="ad-single">
-		<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-		<ins class="adsbygoogle"
-		     style="display:inline-block;width:336px;height:280px"
-		     data-ad-client="ca-pub-1325377879579480"
-		     data-ad-slot="4594956450"></ins>
-		<script>
-		(adsbygoogle = window.adsbygoogle || []).push({});
-		</script>
+		<iframe src="http://rcm-fe.amazon-adsystem.com/e/cm?t=hajipion-22&o=9&p=12&l=ur1&category=design&banner=02KKMDVTR81BAKEFR302&f=ifr" width="300" height="250" scrolling="no" border="0" marginwidth="0" style="border:none;" frameborder="0"></iframe>
 	</div>
 	<div class="ad-single">
-		<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-		<ins class="adsbygoogle"
-		     style="display:inline-block;width:336px;height:280px"
-		     data-ad-client="ca-pub-1325377879579480"
-		     data-ad-slot="4594956450"></ins>
-		<script>
-		(adsbygoogle = window.adsbygoogle || []).push({});
-		</script>
+		<iframe src="http://rcm-fe.amazon-adsystem.com/e/cm?t=hajipion-22&o=9&p=12&l=ur1&category=pc_accessories&banner=04B42GFTZJJN16744BR2&f=ifr" width="300" height="250" scrolling="no" border="0" marginwidth="0" style="border:none;" frameborder="0"></iframe>
 	</div>
 </div>
--->
