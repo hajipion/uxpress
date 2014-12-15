ede3101b637320ee1c5f3366b2560861b41f0eaa
hajipion <hajiming@gmail.com>
2014-12-16 00:45:22 +0900

    Exchange ad location

--- tmp-ad-single.php
+++ tmp-ad-single.php
@@ -1,9 +1,9 @@
 <div class="single-ad-area">
 	<p class="recommend"><small>（UXPress オススメの本↓）</small></p>
 	<div class="ad-single">
-		<SCRIPT charset="utf-8" type="text/javascript" src="http://ws-fe.amazon-adsystem.com/widgets/q?rt=tf_ssw&ServiceVersion=20070822&MarketPlace=JP&ID=V20070822%2FJP%2Fhajipion-22%2F8003%2Ff30712ca-29ad-45f5-8291-983228e0d771&Operation=GetScriptTemplate"> </SCRIPT> <NOSCRIPT><A HREF="http://ws-fe.amazon-adsystem.com/widgets/q?rt=tf_ssw&ServiceVersion=20070822&MarketPlace=JP&ID=V20070822%2FJP%2Fhajipion-22%2F8003%2Ff30712ca-29ad-45f5-8291-983228e0d771&Operation=NoScript">Amazon.co.jp ウィジェット</A></NOSCRIPT>
+		<iframe src="http://rcm-fe.amazon-adsystem.com/e/cm?t=hajipion-22&o=9&p=12&l=ur1&category=design&banner=02KKMDVTR81BAKEFR302&f=ifr" width="300" height="250" scrolling="no" border="0" marginwidth="0" style="border:none;" frameborder="0"></iframe>
 	</div>
 	<div class="ad-single">
-		<iframe src="http://rcm-fe.amazon-adsystem.com/e/cm?t=hajipion-22&o=9&p=12&l=ur1&category=design&banner=02KKMDVTR81BAKEFR302&f=ifr" width="300" height="250" scrolling="no" border="0" marginwidth="0" style="border:none;" frameborder="0"></iframe>
+		<SCRIPT charset="utf-8" type="text/javascript" src="http://ws-fe.amazon-adsystem.com/widgets/q?rt=tf_ssw&ServiceVersion=20070822&MarketPlace=JP&ID=V20070822%2FJP%2Fhajipion-22%2F8003%2Ff30712ca-29ad-45f5-8291-983228e0d771&Operation=GetScriptTemplate"> </SCRIPT> <NOSCRIPT><A HREF="http://ws-fe.amazon-adsystem.com/widgets/q?rt=tf_ssw&ServiceVersion=20070822&MarketPlace=JP&ID=V20070822%2FJP%2Fhajipion-22%2F8003%2Ff30712ca-29ad-45f5-8291-983228e0d771&Operation=NoScript">Amazon.co.jp ウィジェット</A></NOSCRIPT>
 	</div>
 </div>
