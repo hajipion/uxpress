702e4d3bc2d2186ddcd23b66918f2b514da07aaf
hajipion <hajiming@gmail.com>
2015-02-20 01:27:50 +0900

    add big ad

--- tmp-ad-single.php
+++ tmp-ad-single.php
@@ -5,4 +5,7 @@
 	<div class="ad-single">
 		<a href="http://c.af.moshimo.com/af/c/click?a_id=485221&p_id=924&pc_id=1151&pl_id=12213&guid=ON" target="_blank"><img src="http://image.moshimo.com/af-img/0278/000000012213.png" width="300" height="250" style="border:none;"></a><img src="http://i.af.moshimo.com/af/i/impression?a_id=485221&p_id=924&pc_id=1151&pl_id=12213" width="1" height="1" style="border:none;">
 	</div>
+	<div class="ad-single">
+		<a href="http://c.af.moshimo.com/af/c/click?a_id=485134&p_id=926&pc_id=1171&pl_id=12236&guid=ON" target="_blank"><img src="http://image.moshimo.com/af-img/0280/000000012236.jpg" width="600" height="500" style="border:none;"></a><img src="http://i.af.moshimo.com/af/i/impression?a_id=485134&p_id=926&pc_id=1171&pl_id=12236" width="1" height="1" style="border:none;">
+	</div>
 </div>
