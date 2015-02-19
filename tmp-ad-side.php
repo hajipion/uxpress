f51fe516008e3ec10a16f361ad278b2155f3274f
hajipion <hajiming@gmail.com>
2015-02-19 09:24:03 +0900

    update ads

--- tmp-ad-side.php
+++ tmp-ad-side.php
@@ -9,6 +9,6 @@
 		<iframe src="http://rcm-fe.amazon-adsystem.com/e/cm?lt1=_blank&bc1=C3C3C3&IS2=1&bg1=FFFFFF&fc1=000000&lc1=5199EF&t=hajipion-22&o=9&p=8&l=as1&m=amazon&f=ifr&ref=tf_til&asins=B00MVLKM2W" style="width:237px;height:240px;" scrolling="no" marginwidth="0" marginheight="0" frameborder="0"></iframe>
 	</div>
 	<div class="ad-side">
-		<iframe src="http://rcm-fe.amazon-adsystem.com/e/cm?t=hajipion-22&o=9&p=11&l=ur1&category=amazonsale&f=ifr" width="237" height="600" scrolling="no" border="0" marginwidth="0" style="border:none;" frameborder="0"></iframe>
+		<a href="http://c.af.moshimo.com/af/c/click?a_id=485221&p_id=924&pc_id=1151&pl_id=12187&guid=ON" target="_blank"><img src="http://image.moshimo.com/af-img/0278/000000012187.jpg" width="160" height="600" style="border:none;"></a><img src="http://i.af.moshimo.com/af/i/impression?a_id=485221&p_id=924&pc_id=1151&pl_id=12187" width="1" height="1" style="border:none;">
 	</div>
 </div>
--- tmp-ad-single.php
+++ tmp-ad-single.php
@@ -1,9 +1,8 @@
 <div class="single-ad-area">
-	<p class="recommend"><small>（UXPress オススメの本↓）</small></p>
 	<div class="ad-single">
-		<iframe src="http://rcm-fe.amazon-adsystem.com/e/cm?t=hajipion-22&o=9&p=12&l=ur1&category=design&banner=02KKMDVTR81BAKEFR302&f=ifr" width="300" height="250" scrolling="no" border="0" marginwidth="0" style="border:none;" frameborder="0"></iframe>
+		<a href="http://c.af.moshimo.com/af/c/click?a_id=485218&p_id=914&pc_id=1138&pl_id=12104&guid=ON" target="_blank"><img src="http://image.moshimo.com/af-img/0275/000000012104.jpg" width="300" height="250" style="border:none;"></a><img src="http://i.af.moshimo.com/af/i/impression?a_id=485218&p_id=914&pc_id=1138&pl_id=12104" width="1" height="1" style="border:none;">
 	</div>
 	<div class="ad-single">
-		<SCRIPT charset="utf-8" type="text/javascript" src="http://ws-fe.amazon-adsystem.com/widgets/q?rt=tf_ssw&ServiceVersion=20070822&MarketPlace=JP&ID=V20070822%2FJP%2Fhajipion-22%2F8003%2Ff30712ca-29ad-45f5-8291-983228e0d771&Operation=GetScriptTemplate"> </SCRIPT> <NOSCRIPT><A HREF="http://ws-fe.amazon-adsystem.com/widgets/q?rt=tf_ssw&ServiceVersion=20070822&MarketPlace=JP&ID=V20070822%2FJP%2Fhajipion-22%2F8003%2Ff30712ca-29ad-45f5-8291-983228e0d771&Operation=NoScript">Amazon.co.jp ウィジェット</A></NOSCRIPT>
+		<a href="http://c.af.moshimo.com/af/c/click?a_id=485221&p_id=924&pc_id=1151&pl_id=12213&guid=ON" target="_blank"><img src="http://image.moshimo.com/af-img/0278/000000012213.png" width="300" height="250" style="border:none;"></a><img src="http://i.af.moshimo.com/af/i/impression?a_id=485221&p_id=924&pc_id=1151&pl_id=12213" width="1" height="1" style="border:none;">
 	</div>
 </div>
